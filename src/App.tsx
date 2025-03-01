0bb1933f6d094c9180093ce552f4b21cb073c7b0
BETTERGanking <nsagexb@gmail.com>
2025-03-01 19:50:50 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-
 import './App.css';
 
 function App() {
   return (
     <>
-      <div className="container" style={{ textAlign: 'center' }}>
-        <iframe 
-          width="1280" 
-          height="720" 
-          src="https://www.youtube.com/embed/D_MUbs9dnDU" 
-          title="Watch for a free rank" 
-          frameBorder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-          referrerPolicy="strict-origin-when-cross-origin" 
-          allowFullScreen
-        ></iframe>
+      <div className="container">
+        <div className="video-wrapper">
+          <iframe 
+            src="https://www.youtube.com/embed/D_MUbs9dnDU" 
+            title="Watch for a free rank" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerPolicy="strict-origin-when-cross-origin" 
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
     </>
   );
