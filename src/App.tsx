589170cc6e27fad454ec32c82beb747607212578
BETTERGanking <nsagexb@gmail.com>
2025-03-01 19:57:38 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
           referrerPolicy="strict-origin-when-cross-origin" 
           allowFullScreen
-        ></iframe>
+        ></iframe> <p>po update</p>
       </div>
     </>
   );
